35c1a1166e74f00642a5e1f4301c011aa0731e70
aarchamb <anthony85180@gmail.com>
2016-12-13 13:10:26 +0100

    Modification header

--- public/app/Home/home.controller.js
+++ public/app/Home/home.controller.js
@@ -10,8 +10,9 @@
 
     function HomeController($scope, $location ,$http, $rootScope, ngDialog,ServiceStory) {
 
-        $scope.message= " Bienvenue sur ce super site !! "
-
+        $scope.message= " Bienvenue sur l'application Storym, dont la finalité est de créer " +
+            "des histoires intéractives à la manière des ' livre dont vous êtes " +
+            "le héro ' accèssibles depuis une application mobile actuellement en développement (Ionic 2). "
     }
 
 })();
