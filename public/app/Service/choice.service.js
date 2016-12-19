0b966d20d29dcd1ee0c015ef5c3c2261b76f7b35
aarchamb <anthony85180@gmail.com>
2016-12-19 16:26:23 +0100

    Création service choix

---
+++ public/app/Service/choice.service.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+    angular
+        .module('Storym')
+        .factory('ServiceChoice',ServiceChoice);
+
+    ServiceChoice.$inject = ['$resource','$rootScope','$http','ConfigService'];
+
+    function ServiceChoice($resource, $rootScope,$http,ConfigService) {
+
+        return {
+            getChoice : getChoice,
+            getChoices : getChoices,
+            updateChoice : updateChoice,
+            createChoice : createChoice,
+            removeChoice : removeChoice,
+            castPageChoice : castPageChoice
+        };
+
+        // castPageChoice take an id parameter en return choice object
+
+        function castPageChoice(idChapitre) {
+            return getChoice(idChapitre).$promise.then(function (choice) {
+                return choice;
+            });
+        }
+
+        function getChoices() {
+            return $http.get(ConfigService.getApiUrl()+'/choice')
+        }
+
+        function getChoice(id) {
+            return $resource(ConfigService.getApiUrl()+"/choice/"+id).get();
+        }
+
+        function updateChoice(choice) {
+            $resource(ConfigService.getApiUrl()+'/choice/'+choice.id).save(choice);
+        }
+
+        function createChoice(choice) {
+            $resource(ConfigService.getApiUrl()+'/choice').save(choice);
+        }
+
+        function removeChoice(choice) {
+            $resource(ConfigService.getApiUrl()+'/choice/'+choice.id).remove();
+        }
+
+    }
+})();
