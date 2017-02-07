9e52ad2da145c5813c7539e36fd297c7ccbd35a8
aarchamb <anthony85180@gmail.com>
2017-02-07 11:10:36 +0100

    Modification page fonctionelle

--- public/app/Modales/Page/updatePage/modale.updatePage.controller.js
+++ public/app/Modales/Page/updatePage/modale.updatePage.controller.js
@@ -11,32 +11,46 @@
 
 
         $scope.initModalUpdate = function(){
-            $scope.pageToUpdate = $rootScope.selectedPage;
+            // $scope.pageToUpdate = $rootScope.selectedPage;
         };
         
         $scope.pageToUpdate = $rootScope.selectedPage;
 
-        // console.log($scope.pageToUpdate)
-        // $scope.pageToUpdate.choice1 = $scope.pageToUpdate.choices[0].label;
-        // $scope.pageToUpdate.choice2 = $scope.pageToUpdate.choices[1].label;
-
         $scope.update = function(pageToUpdate){
             if(pageToUpdate){
-                pageToUpdate.choices = [];
-                if($scope.choice1  ) {
-                    pageToUpdate.choices.push($scope.pageToUpdate.choice1);
+                if($scope.pageToUpdate.choice1) {
+                    pageToUpdate.choices[0] = $scope.pageToUpdate.choice1;
+                    if($scope.pageToUpdate.choices[1] && $scope.pageToUpdate.choices[1].id) {
+                        pageToUpdate.choices[1] = $scope.pageToUpdate.choices[1].id;
+                    }
                 }
-                if($scope.choice2){
-                    pageToUpdate.choices.push($scope.pageToUpdate.choice2);
+                if($scope.pageToUpdate.choice2){
+                    pageToUpdate.choices[1] =  $scope.pageToUpdate.choice2 ;
+                    if($scope.pageToUpdate.choices[0] && $scope.pageToUpdate.choices[0].id) {
+                        pageToUpdate.choices[0] = $scope.pageToUpdate.choices[0].id;
+                    }
                 }
+                delete $scope.pageToUpdate.choice2;
+                delete $scope.pageToUpdate.choice1;
 
-                ServicePage.cupdatePage(pageToUpdate);
+                ServicePage.updatePage(pageToUpdate);
                 ngDialog.close();
                 $rootScope.reload = true;
             }
         };
 
 
+        $scope.showChoice1Field = false;
+        $scope.showChoice2Field = false;
+
+        $scope.clickChoice1 =function(){
+            $scope.showChoice1Field =  !$scope.showChoice1Field;
+        };
+
+        $scope.clickChoice2 =function(){
+            $scope.showChoice2Field =  !$scope.showChoice2Field;
+            console.log($scope.pageToUpdate)
+        };
 
         ServiceChoice.getChoices().success(function (choices) {
             $scope.allChoices = choices;
