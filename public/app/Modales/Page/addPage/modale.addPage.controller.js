29baf672750f35c896908659626c37d01ae92d81
aarchamb <anthony85180@gmail.com>
2017-02-07 13:01:23 +0100

    Creation de choix  fonctionnelle dans modale ajout page

--- public/app/Modales/Page/addPage/modale.addPage.controller.js
+++ public/app/Modales/Page/addPage/modale.addPage.controller.js
@@ -10,31 +10,45 @@
 
     function modaleAjoutPageController($scope, $location ,$http, $rootScope, ngDialog,ServicePage,$route,ServiceChoice,ServiceChapter) {
 
+        $scope.showCreateChoice = false;
+        updateAll();
 
-            $scope.initModalAdd = function(){
-                $scope.newPage = {}
-            };
+        $scope.initModalAdd = function(){
+            $scope.newPage = {}
+        };
 
-            $scope.add = function(newPage){
-                if(newPage){
-                    newPage.choices = [];
-                    if($scope.choice1 && $scope.choice2) {
-                        newPage.choices.push($scope.choice1);
-                        newPage.choices.push($scope.choice2);
-                        ServicePage.createPage(newPage);
-                        ngDialog.close();
-                        $rootScope.reload = true;
-                    }else{
-                        alert("Vous n'avez pas sélectionné les deux choix possibles de la page à créer")
-                    }
+        $scope.add = function(newPage){
+            if(newPage){
+                newPage.choices = [];
+                if($scope.choice1 && $scope.choice2) {
+                    newPage.choices.push($scope.choice1);
+                    newPage.choices.push($scope.choice2);
+                    ServicePage.createPage(newPage);
+                    ngDialog.close();
+                    $rootScope.reload = true;
+                }else{
+                    alert("Vous n'avez pas sélectionné les deux choix possibles de la page à créer")
                 }
-            };
+            }
+        };
 
+        $scope.createChoice = function(choice){
+            if(choice.label) {
+                ServiceChoice.createChoice(choice)
+                updateAll();
+                $scope.showCreateChoice = false;
+            }
+        };
 
+        $scope.clickShowCreateChoice =function(){
+            $scope.showCreateChoice= !$scope.showCreateChoice;
+        };
 
-        ServiceChoice.getChoices().success(function (choices) {
-            $scope.allChoices = choices;
-        });
+        function updateAll() {
+            ServiceChoice.getChoices().success(function (choices) {
+                $scope.allChoices = choices;
+            });
+        }
 
         ServiceChapter.getChapters().success(function (chapters) {
             $scope.allChapters = chapters;
